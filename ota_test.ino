b49bdd114658a7df7b4c29af59468441e663a730
mx3030 <mxpayne3030@gmail.com>
2024-09-07 23:00:19 +0200

    fast

--- ota_test.ino
+++ ota_test.ino
@@ -1,17 +1,16 @@
 #include "WiFi_Module.h"
 #include "OTA_Module.h"
 
-const char *firmwareURL = "http://192.168.178.86/ota_test.ino.bin"; 
-
 void setup() {
     Serial.begin(115200);
-    WiFi_Module::connect(DEFAULT_NETWORK_SETTINGS);
+    //WiFi_Module::connect(DEFAULT_NETWORK_SETTINGS);
+    WiFi_Module::connect(WLAN_7270_SETTINGS);
     OTA_Module::init(); 
     OTA_Module::initServer();
 }
 
 void loop() {
-    OTA_Module::handleOTAClient();
+    OTA_Module::otaServer->handleClient();
     Serial.println("Test");
     delay(1000);
 }
